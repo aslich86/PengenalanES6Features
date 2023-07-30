725191465b8b44557699cbfded36421a22e3c6b7
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-30 09:42:56 +0700

    Create Template Literal.js

---
+++ Template Literal.js
@@ -0,0 +1,8 @@
+const name = "John";
+const age = 30;
+
+// Tanpa template literal
+console.log("Nama: " + name + ", Usia: " + age);
+
+// Dengan template literal
+console.log(`Nama: ${name}, Usia: ${age}`);
