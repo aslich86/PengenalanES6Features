caaf2944f9c44eabea05c0b2ce5ec0e6ce337c82
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-30 09:43:22 +0700

    Create Destructing.js

---
+++ Destructing.js
@@ -0,0 +1,12 @@
+// Destructuring objek
+const person = { name: "John", age: 30 };
+const { name, age } = person;
+console.log(name); // Output: "John"
+console.log(age); // Output: 30
+
+// Destructuring array
+const numbers = [1, 2, 3];
+const [x, y, z] = numbers;
+console.log(x); // Output: 1
+console.log(y); // Output: 2
+console.log(z); // Output: 3
